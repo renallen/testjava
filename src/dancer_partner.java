4dd78e9656227a9d5ff71cd9da51ed6d36448c67
renallen <845839703@qq.com>
2018-08-26 15:24:17 +0800

    update 判断是否是稳定匹配

--- src/dancer_partner.java
+++ src/dancer_partner.java
@@ -29,36 +29,58 @@ public class dancer_partner {
 
 	static int getalone(partner[] boys,int Count) {
 		//找到没有匹配的男孩
-		for(int i=0;i<Count;i++) {
+		
+		for( int i=0;i<Count; i++) {
 			if(boys[i].current==-1) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	static boolean Isperfect(partner p,int gcid,int bid) {
-		//判断更喜欢哪个
+	static int getpos(partner p,int id) {
 		for(int i=0;i<p.Count;i++) {
-			if(p.Plist[i]==gcid){
-				return false;
-			}
-			if(p.Plist[i]==bid){
-				return true;
+			if(p.Plist[i]==id){
+				return i;
 			}
 		}
+		return p.Count;
+	}
+	static boolean Isperfect(partner p,int gcid,int bid) {
+		//判断更喜欢哪个
+
+		if(getpos(p,gcid)>getpos(p,bid))
+			return true;
 		
 		return false;
 	}
-
+	static boolean ustable(partner[] boys,partner[] girls,int Count) {
+			for(int i=0;i<Count;i++) {
+				int gpos=getpos(boys[i],boys[i].current);
+				for(int k=0;k<gpos;k++) {
+					int gid=boys[i].Plist[k];
+					int cpos=getpos(girls[gid],girls[gid].current);
+					int bpos=getpos(girls[gid],i);
+					if(bpos<cpos) {
+						return true;
+					}
+				}
+			}
+			return false;
+	}
 	public static void main(String []arg) {
 			Count=3;
-			boys= new  partner[] {new partner(-1,0,3,new int[]{0,1,2}),new partner(-1,0,3,new int[]{0,1,2}),new partner(-1,0,3,new int[]{1,0,2})};
-			girls= new  partner[] {new partner(-1,0,3,new int[]{0,2,1}),new partner(-1,0,3,new int[]{2,0,1}),new partner(-1,0,3,new int[]{0,2,1})};
+			boys= new  partner[] {new partner(-1,0,3,new int[]{0,2,1}),new partner(-1,0,3,new int[]{1,2,0}),new partner(-1,0,3,new int[]{0,1,2})};
+			girls= new  partner[] {new partner(-1,0,3,new int[]{2,0,1}),new partner(-1,0,3,new int[]{0,2,1}),new partner(-1,0,3,new int[]{1,0,2})};
+			
 			match_partner(boys,girls,Count);
 			for(int i=0;i<Count;i++) {
-				System.out.println("第"+i +"个匹配："+boys[i].current+" "+girls[boys[i].current].current);
+				System.out.println("第"+i +"个匹配："+boys[i].current+" "
+			+getpos(boys[i],boys[i].current)+" "+girls[boys[i].current].current+" "
+						+getpos(girls[boys[i].current],girls[boys[i].current].current) );
 			}
-			
+			if(ustable(boys,girls,Count)) {
+				System.out.println("ustable");
+			}
 	}
 
 }
